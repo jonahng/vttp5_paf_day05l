97bad0f2f97f3eac2cc4d0208de6241c32c632d4
jonahng <nyhj1998@gmail.com>
2025-01-14 13:36:09 +0800

    pm lesson

---
+++ vttp5_paf_day5lpm/src/main/java/com/jonah/vttp5_paf_day5lpm/Vttp5PafDay5lpmApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_paf_day5lpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5PafDay5lpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5PafDay5lpmApplication.class, args);
+	}
+
+}
---
+++ vttp5_paf_day5lpm/src/test/java/com/jonah/vttp5_paf_day5lpm/Vttp5PafDay5lpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_paf_day5lpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5PafDay5lpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
